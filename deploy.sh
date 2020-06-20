4b0f7866741943597c421e486b7a642d24704dc9
Thomas Claireau <thomas.claireau@gmail.com>
2020-06-20 10:45:45 +0200

    deploy in cpanel OK

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,25 @@
+#!/bin/bash
+
 echo "Quel est le titre du dépot ?"
 read title
 echo "Une description pour le dépot ?"
 read description
 echo "Quel est le slug du dépot ?"
 read slug
+echo "A quel niveau créer le repository local ?"
+read path
+
+cd $path
+if (( $? )) ; then exit ; fi
+
 mkdir $slug
+if (( $? )) ; then exit ; fi
+
 cd $slug
+if (( $? )) ; then exit ; fi
 
 # Git Config
 git init
-
 git remote add origin https://github.com/thomas-claireau/$slug.git
 
 while true; do
@@ -20,36 +30,40 @@ while true; do
 		git remote set-url --add --push origin https://github.com/thomas-claireau/$slug.git
 		git remote set-url --add --push origin ssh://clth9110@thomas-claireau.fr/home/clth9110/repositories/$slug
 
-		echo "Sous dossier ?"
-		read sousdossier
+		echo "chemin et avec slash à la fin (sans le dossier final)"
+		read chemin
 
-		# Cpanel Config
-		cat <<EOT >>.cpanel.yml
+		echo "nom du dossier final sur le serveur (sans slash)"
+		read dossier_final
+
+# Cpanel Config
+cat <<EOT >>.cpanel.yml
 deployment:
-	tasks:
-		- export DEPLOYPATH=~/$sousdossier$slug
-		- /bin/cp -R * \$DEPLOYPATH
-		- \$DEPLOYPATH/build.sh 2>&1
+  tasks:
+    - export DEPLOYPATH=~/$chemin$dossier_final
+    - /bin/cp -R * \$DEPLOYPATH
+    - \$DEPLOYPATH/build.sh 2>&1
 EOT
 
-		# Build Config
-		cat <<EOT >>build.sh
+# Build Config
+cat <<EOT >>build.sh
 #!/bin/bash
 set -x
 
 # Build script
-cd ~/$sousdossier$slug
+cd ~/$chemin$dossier_final
 source ~/.bashrc
 npm install
 npm run build
 EOT
 
-		chmod +x build.sh
-
-		# Create empty folder project in cpanel server
+		# Create empty folder project in cpanel server (+ create empty build.sh executable)
 		ssh clth9110@thomas-claireau.fr <<EOF
-cd ~/$sousdossier
-mkdir $slug
+cd ~/$chemin
+mkdir $dossier_final
+cd $dossier_final
+touch build.sh
+chmod +x build.sh
 exit
 EOF
 		break
@@ -68,9 +82,9 @@ $description
 EOT
 
 # Git Actions
-# git add .
-# git commit -m "First commit"
-# git push -u origin master
+git add .
+git commit -m "First commit"
+git push -u origin master
 
-git checkout -b dev
+# git checkout -b dev
 # git push origin dev
